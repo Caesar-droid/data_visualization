6ff0583bc301b31e8ea5e2624d01697d6d054ce7
domie <dominictoroitichh@gmail.com>
2021-04-07 21:57:52 +0300

    correcting an error

--- die.py
+++ die.py
@@ -4,8 +4,8 @@ class Die:
     """A class representing a single die."""
     def __init__(self,num_sides=6):
         """Assume a six-sided die."""
-        self.nun_sides=num_sides
+        self.num_sides=num_sides
     def roll(self):
         """Return a random value between 1 and number of sides."""
-        return randint(1,self.nun_sides)
+        return randint(1,self.num_sides)
     
