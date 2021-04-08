e6efd29873dd3087af042e91455baf29e55b2db7
domie <dominictoroitichh@gmail.com>
2021-04-08 13:58:06 +0300

    analyzing the results

--- die_visual.py
+++ die_visual.py
@@ -5,8 +5,13 @@ die=Die()
 
 #make some rolls, and store results in a list.
 results=[]
-for roll_num in range(100):
+for roll_num in range(1000):
     result=die.roll()
     results.append(result)
+#Analyze the results.
+frequencies =[]
+for value in range(1,die.num_sides+1):
+    frequency=results.count(value)
+    frequencies.append(frequency)
 
-print(results)
+print(frequencies)
