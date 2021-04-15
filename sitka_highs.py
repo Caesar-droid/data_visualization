cf3f5a9f9523613b32cb4fb2546e3f2c6d9abf22
domie <dominictoroitichh@gmail.com>
2021-04-15 07:54:07 +0300

    extracting and reading data

--- sitka_highs.py
+++ sitka_highs.py
@@ -6,5 +6,11 @@ with open(filename) as f:
     header_row=next(reader)
     # print(header_row)
     
-    for index,column_header in enumerate(header_row):
-        print(index,column_header)
+    # for index,column_header in enumerate(header_row):
+    #     print(index,column_header)
+    #Get high temperatures from this file.
+    highs=[]
+    for row in reader:
+        high=int(row[5])
+        highs.append(high)
+print(highs)
