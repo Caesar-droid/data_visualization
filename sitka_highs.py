a6fe3a7494f0b26cd8efd55d053087dcd250a3ca
domie <dominictoroitichh@gmail.com>
2021-04-19 07:21:19 +0300

    plotting a longer timeframe

--- sitka_highs.py
+++ sitka_highs.py
@@ -2,7 +2,7 @@ import csv
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-filename='data/sitka_weather_07-2018_simple.csv'
+filename='data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader=csv.reader(f)
     header_row=next(reader)
@@ -29,7 +29,7 @@ ax.plot(dates, highs,c='red')
 
 #Format plot.
 
-plt.title('Daily high temperatures, July 2018',fontsize=24)
+plt.title('Daily high temperatures -2018',fontsize=24)
 plt.xlabel('',fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature(F)",fontsize=16)
