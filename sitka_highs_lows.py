faad2a1f5deb7dc57c8830510b71979442a63ede
domie <dominictoroitichh@gmail.com>
2021-04-20 19:43:07 +0300

    shading an area in the chart

--- sitka_highs_lows.py
+++ sitka_highs_lows.py
@@ -23,8 +23,9 @@ print(highs)
 
 plt.style.use('seaborn')
 fig, ax =plt.subplots()
-ax.plot(dates, highs,c='red')
-ax.plot(dates,lows,c='blue')
+ax.plot(dates, highs,c='red',alpha=0.5)
+ax.plot(dates,lows,c='blue',alpha=0.5)
+plt.fill_between(dates,highs,lows,facecolor='blue',alpha=0.1)
 
 #Format plot.
 
