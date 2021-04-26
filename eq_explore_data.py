86c273fd3d97e1bd03606b1268145e1b61b7d153
domie <dominictoroitichh@gmail.com>
2021-04-26 08:06:47 +0300

    extracting location data

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -11,8 +11,15 @@ with open (filename) as f:
 all_eq_dicts = all_eq_data['features']
 # print(len(all_eq_dicts))
 
-mags=[]
+mags,lons,lats=[],[],[]
 for eq_dict in all_eq_dicts:
     mag=eq_dict['properties']['mag']
+    lon=eq_dict['geometry']['coordinates'][0]
+    lat=eq_dict['geometry']['coordinates'][1]
     mags.append(mag)
-print(mags[:10])    
+    lons.append(lon)
+    lats.append(lat)
+print(mags[:10])
+print(lons[:5])
+print(lats[:5])    
+    
