35f587503f5678c46ca0c611cf9b0cc06dccac26
domie <dominictoroitichh@gmail.com>
2021-05-30 13:34:44 +0300

    customizing marker size

--- eq_world_map.py
+++ eq_world_map.py
@@ -26,6 +26,9 @@ data=[{
     'type': 'scattergeo',
     'lon':lons,
     'lat':lats,
+    'marker': {
+        'size':[5*mag for mag in mags],
+    },
 }]
 my_layout =Layout (title ='Global Earthquakes')
 
