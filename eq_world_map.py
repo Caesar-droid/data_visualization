34461102415ceaa4ddd92017cca9a4eb861933fb
domie <dominictoroitichh@gmail.com>
2021-05-30 14:28:03 +0300

    adding hover text

--- eq_world_map.py
+++ eq_world_map.py
@@ -1,6 +1,7 @@
 import json
 from plotly.graph_objs import Scattergeo , Layout
 from plotly import offline
+
 #Explore the structure of the data.
 filename ='data/eq_data_30_day_m1.json'
 with open (filename) as f:
@@ -12,20 +13,23 @@ with open (filename) as f:
 all_eq_dicts = all_eq_data['features']
 # print(len(all_eq_dicts))
 
-mags,lons,lats=[],[],[]
+mags,lons,lats, hover_texts=[],[],[],[]
 for eq_dict in all_eq_dicts:
     mag=eq_dict['properties']['mag']
     lon=eq_dict['geometry']['coordinates'][0]
     lat=eq_dict['geometry']['coordinates'][1]
+    title =eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 
 #Map the earthquakes.
 data=[{
     'type': 'scattergeo',
     'lon':lons,
     'lat':lats,
+    'text':hover_texts,
     'marker': {
         'size':[5*mag for mag in mags],
         'color': mags,
