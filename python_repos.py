0ba1593311dfe74789e23a490ba4b80eb5128022
Caesar-droid <dominictoroitichh@gmail.com>
2021-10-05 16:08:20 +0300

    working with the response dictionary

--- python_repos.py
+++ python_repos.py
@@ -8,6 +8,26 @@ print(f"Status code:{r.status_code}")
 
 #Store API response in a variable.
 response_dict=r.json()
+print(f"Total repositories: {response_dict['total_count']}")
 
-#Process results.
-print(response_dict.keys())
+#Explore information about the repositories.
+repo_dicts=response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+#examine the first repository.
+repo_dict=repo_dicts[0]
+# print(f"\nKeys: {len(repo_dict)}")
+# for key in sorted(repo_dict.keys()):
+#     print(key)
+print("\nSelected information about first repository:")
+print(f"Name: {repo_dict['name']}")
+print(f"Owner: {repo_dict['owner']['login']}")
+print(f"Stars: {repo_dict['stargazers_count']}")
+print(f"Repository: {repo_dict['html_url']}")
+print(f"Created: {repo_dict['created_at']}")
+print(f"Updated: {repo_dict['updated_at']}")
+print(f"Description: {repo_dict['description']}")
+
+
+
+
