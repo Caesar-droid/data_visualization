47dc09a66f93569dbc32eb49a99d2d9e5465d347
Caesar-droid <dominictoroitichh@gmail.com>
2021-10-05 19:29:15 +0300

    adding clickable links to our graph

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -13,18 +13,21 @@ print(f"Status code:{r.status_code}")
 #process results
 response_dict=r.json()
 repo_dicts=response_dict['items']
-repo_names,stars,labels=[],[],[]
+repo_links,stars,labels=[],[],[]
 for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
+    repo_name=(repo_dict['name'])
+    repo_url=repo_dict['html_url']
+    repo_link=f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
     stars.append(repo_dict['stargazers_count'])
     owner=repo_dict['owner']['login']
     description=repo_dict['description']
-    label= f"{owner}<br/>{description}"
+    label= f"{owner} <br>{description}"
     labels.append(label)
 #make visualization
 data =[{
     'type': 'bar',
-    'x': repo_names,
+    'x': repo_links,
     'y':stars,
     'hovertext':labels,
     'marker':{
